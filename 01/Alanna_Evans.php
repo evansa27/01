fc3f2ab5fa841104ad95c686c255e80facbbfbef
evansa27 <alanna71103@gmail.com>
2024-09-02 19:28:00 -0400

    first commit

---
+++ 01/Alanna_Evans.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <title>Alanna Evans's Resume</title>
+    
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Your name's resume">
+    <meta name="author" content="Your name">    
+    <link rel="shortcut icon" href="favicon.ico"> 
+    
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+    
+    <!-- FontAwesome JS-->
+	<script defer src="assets/fontawesome/js/all.min.js"></script>
+       
+    <!-- Theme CSS -->  
+    <link id="theme-style" rel="stylesheet" href="assets1/css/pillar-1.css">
+
+
+</head> 
+
+<body>
+    <article class="resume-wrapper text-center position-relative">
+	    <div class="resume-wrapper-inner mx-auto text-start bg-white shadow-lg">
+		    <header class="resume-header pt-4 pt-md-0">
+			    <div class="row">
+				    <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
+				        <img class="picture" src="Evans_Alanna.jpg" alt="Headshot of Alanna Evans">
+				    </div><!--//col-->
+				    <div class="col">
+					    <div class="row p-4 justify-content-center justify-content-md-between">
+						    <div class="primary-info col-auto">
+								<h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase"> <?php echo "Alanna Evans";?></h1>
+							    <div class="title mb-3"> <?php echo "Digital Technology Leadership Program at GE Aerospace";?></div>
+							    <ul class="list-unstyled">
+								    <li class="mb-2"><a class="text-link" href="#"><i class="far fa-envelope fa-fw me-2" data-fa-transform="grow-3"></i><?php echo "evansa27@mymail.nku.edu";?></a></li>
+								    <li><a class="text-link" href="#"><i class="fas fa-mobile-alt fa-fw me-2" data-fa-transform="grow-6"></i><?php echo "0123 456 78900";?></a></li>
+							    </ul>
+						    </div><!--//primary-info-->
+						    <div class="secondary-info col-auto mt-2">
+							    <ul class="resume-social list-unstyled">
+					                <li class="mb-3"><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fab fa-linkedin-in fa-fw"></i></span><?php echo "linkedin.com/in/yourlink";?></a></li>
+					                <li class="mb-3"><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fab fa-github-alt fa-fw"></i></span><?php echo "github.com/yourhandle";?></a></li>
+							    </ul>
+						    </div><!--//secondary-info-->
+					    </div><!--//row-->
+					    
+				    </div><!--//col-->
+			    </div><!--//row-->
+		    </header>
+		    <div class="resume-body p-5">
+			    <section class="resume-section summary-section mb-5">
+				    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo "Summary";?></h2>
+				    <div class="resume-section-content">
+					    <p class="mb-0"><?php echo "My name is Alanna Evans, and I am a senior working on my Bachelor of Science in Cybersecurity at Northern Kentucky University. I am also minoring in Computer Science and Computer Forensics as well. I have had multiple cybersecurtrity based internships at GE Aerospace. During my time here I worked on the Insider Threat and Data Protection Team, as well as the Cyber Intel and Active Defense Team. I have recently accpeted a postion as a DTLP within the company after I graduate. A few fun facts about me is that I recently traveled outside of the country for the first time, and I love going to concerts.";?></p>
+				    </div>
+			    </section><!--//summary-section-->
+			    <div class="row">
+				    <div class="col-lg-9">
+					    <section class="resume-section experience-section mb-5"> 
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo "Work Experience";?></h2>
+						    <div class="resume-section-content">
+							    <div class="resume-timeline position-relative">
+								    <article class="resume-timeline-item position-relative pb-5">
+									    
+									    <div class="resume-timeline-item-header mb-2">
+										    <div class="d-flex flex-column flex-md-row">
+										        <h3 class="resume-position-title font-weight-bold mb-1"><?php echo "Detection Engineer";?></h3>
+										        <div class="resume-company-name ms-auto"><?php echo "GE Aerospace";?></div>
+										    </div><!--//row-->
+										    <div class="resume-position-time"><?php echo "May 2024 - August 2024";?></div>
+									    </div><!--//resume-timeline-item-header-->
+									    <div class="resume-timeline-item-desc">
+										    <p><?php echo "-	Led an intern-run threat hunt in which teamwork and collaboration were used to determine if a predetermined threat was a concern within the GE Aerospace network with a focus on certain tactics, techniques, and procedures
+												-	Acted as a threat intel analyst and used Open-Source Intelligence to work through the threat intel pipeline and complete tasks such as conducting open-source intelligence, fulfilling 1 to 2 reports a week, and becoming familiar with threat intelligence tech stacks
+												-	Completed professional development tasks such as attending educational meetings, networking events, and practicing leadership skills by hosting a staff meeting for digital technology interns and preparing for the CIO as a guest speaker";?></p>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold"><?php echo "Achievements:";?></h4>
+										    <p><?php echo "These are some of the achievements I have accomplished during this internship.";?></p>
+										    <ul>
+											    <li><?php echo "Identified ways to improve GE's security";?></li>
+											    <li><?php echo "Got hands on cyber expierence";?></li>
+											    <li><?php echo "Identified ways to improve processes";?></li>
+										    </ul>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold"><?php echo "Technologies used:";?></h4>
+										    <ul class="list-inline">
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo "OpenCTI</span>";?></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo "Cybersecurity Fundamentals<";?>/span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo "BDEP";?></span></li>
+										    </ul>
+									    </div><!--//resume-timeline-item-desc-->
+
+								    </article><!--//resume-timeline-item-->
+								    
+								    <article class="resume-timeline-item position-relative pb-5">
+									    
+									    <div class="resume-timeline-item-header mb-2">
+										    <div class="d-flex flex-column flex-md-row">
+										        <h3 class="resume-position-title font-weight-bold mb-1"><?php echo "Digital Technology Intern on the Insider Threat and Data Protection Team";?></h3>
+										        <div class="resume-company-name ms-auto"><?php echo "GE Aerospace";?></div>
+										    </div><!--//row-->
+										    <div class="resume-position-time"><?php echo "May 2023 - August 2023";?></div>
+									    </div><!--//resume-timeline-item-header-->
+									    <div class="resume-timeline-item-desc">
+										    <p><?php echo "-	Analyzed various threats in a queue that obtained 50-100+ threats per day
+												-	Used critical thinking skills in order to conduct an assessment of each threat to determine if further action was required, as well as communicated necessary information for the escalated cases
+												-	Created an adaptable knowledge base by testing various software, creating mock environments, and then shared my findings with my team until the best format was constructed
+												-	Collaborated within and outside of my team in order to solve problems, expand my knowledge and learn new technical skills, different ways to collect data, and find potential threats";?></p>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold"><?php echo "Achievements";?></h4>
+										    <p><?php echo "I was able to create a working knowledge base as well as go through alerts to determine if that alert was a potential threat.";?></p>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold"><?php echo "Technologies used:";?></h4>
+										    <ul class="list-inline">
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo "Confluence";?></span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo "GE Assets";?></span></li>
+											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?php echo "CSS";?></span></li>
+										    </ul>
+									    </div><!--//resume-timeline-item-desc-->
+
+								    </article><!--//resume-timeline-item-->
+						    </div>
+					    </section><!--//projects-section-->
+				    </div>
+				    <div class="col-lg-3">
+					    <section class="resume-section skills-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo "Skills & Tools";?></h2>
+						    <div class="resume-section-content">
+						        <div class="resume-skill-item">
+							        <ul class="list-unstyled mb-4">
+								        <li class="mb-2">
+								            <div class="resume-skill-name"><?php echo "Leadership";?></div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 98%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name"><?php echo "HTML";?></div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 94%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name"><?php echo "CSS";?></div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 96%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        
+								        <li class="mb-2">
+								            <div class="resume-skill-name"><?php echo "Cybersecurity Knowledge";?></div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 92%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name"><?php echo "Negotiation";?></div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 96%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+							        </ul>
+						        </div><!--//resume-skill-item-->
+						        <div class="resume-skill-item">
+						            <h4 class="resume-skills-cat font-weight-bold"><?php echo "Others";?></h4>
+						            <ul class="list-inline">
+							            <li class="list-inline-item"><span class="badge badge-light"><?php echo "Microsoft Office";?></span></li>
+							            <li class="list-inline-item"><span class="badge badge-light"><?php echo "Word";?></span></li>
+							            <li class="list-inline-item"><span class="badge badge-light"><?php echo "Excel";?></span></li>
+							            <li class="list-inline-item"><span class="badge badge-light"><?php echo "Communication"?></span></li>
+							            <li class="list-inline-item"><span class="badge badge-light"><?php echo "Teamwork";?></span></li>
+						            </ul>
+						        </div><!--//resume-skill-item-->
+						    </div><!--resume-section-content-->
+					    </section><!--//skills-section-->
+					    <section class="resume-section education-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo "Education";?></h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled">
+								    <li class="mb-2">
+								        <div class="resume-degree font-weight-bold"><?php echo "BS in Cybersecurity";?></div>
+								        <div class="resume-degree-org"><?php echo "Northern Kentucky University";?></div>
+								        <div class="resume-degree-time"><?php echo "2021-2025";?></div>
+								    </li>
+								    <li>
+								        <div class="resume-degree font-weight-bold"><?php echo "High School Diploma";?></div>
+								        <div class="resume-degree-org"><?php echo "Bethel-Tate High School";?></div>
+								        <div class="resume-degree-time"><?php echo "2018-2021<";?></div>
+								    </li>
+							    </ul>
+						    </div>
+					    </section><!--//education-section-->
+					    <section class="resume-section reference-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo "Awards";?></h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled resume-awards-list">
+								    <li class="mb-2 ps-4 position-relative">
+								        <i class="resume-award-icon fas fa-trophy position-absolute" data-fa-transform="shrink-2"></i>
+								        <div class="resume-award-name"><?php echo "Fifth Third Scholarship";?></div>
+								        <!-- <div class="resume-award-desc"><?php echo "Recived this due to academic excellence while junior standing in college.";?></div> -->
+								    </li>
+								    <li class="mb-0 ps-4 position-relative">
+								        <i class="resume-award-icon fas fa-trophy position-absolute" data-fa-transform="shrink-2"></i>
+								        <div class="resume-award-name"> <?php echo "EDGE and EDGE Plus Award";?></div>
+								        <div class="resume-award-desc"><?php echo "Recived this due to my high GPA that was maintained throughout high school.";?></div>
+								    </li>
+							    </ul>
+						    </div>
+					    </section><!--//interests-section-->
+					    <section class="resume-section language-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo "Languages";?></h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled resume-lang-list">
+								    <li class="mb-2"><span class="resume-lang-name font-weight-bold"><?php echo "English";?></span> <small class="text-muted font-weight-normal">(Native)</small></li>
+							    </ul>
+						    </div>
+					    </section><!--//language-section-->
+					    <section class="resume-section interests-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo "Interests";?></h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled">
+								    <li class="mb-1"><?php echo "Hiking";?></li>
+								    <li class="mb-1"><?php echo "Playing Pool";?></li>
+								    <li class="mb-1"><?php echo "Video Games";?></li>
+							    </ul>
+						    </div>
+					    </section><!--//interests-section-->
+					    
+				    </div>
+			    </div><!--//row-->
+				<section class="resume-section experience-section mb-5">
+					<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?php echo "Projects";?></h2>
+					<div class="row mt-4">
+						<div class="col-md-4">
+							<div class="card">
+								<img src="path-to-project-image1.jpg" alt="Project 1" class="card-img-top">
+								<div class="card-body">
+									<h5 class="card-title"><?php echo "VR";?></h5>
+									<p class="card-text"><?php echo "Worked closely with a professor to create a tour of the College of Informatics through VR.";?></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card">
+								<img src="path-to-project-image2.jpg" alt="Project 2" class="card-img-top">
+								<div class="card-body">
+									<h5 class="card-title"><?php echo "Cryptography";?></h5>
+									<p class="card-text"><?php echo "Created a Keyword Cipher Software for a crypyography course.";?></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card">
+								<img src="path-to-project-image3.jpg" alt="Project 3" class="card-img-top">
+								<div class="card-body">
+									<h5 class="card-title"><?php echo "Chutes and Ladders";?></h5>
+									<p class="card-text"><?php echo "Wrote software in C that plays the game Chutes and Ladders.";?></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section><!--//projects-section-->
+		    </div><!--//resume-body-->
+		    
+		    
+	    </div>
+    </article> 
+
+    
+    <footer class="footer text-center pt-2 pb-5">
+	    <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
+        <small class="copyright"> <?php echo "Designed with";?> <span class="sr-only">love</span><i class="fas fa-heart"></i> <?php echo "by Alanna Evans";?></small>
+    </footer>
+
+    
+
+</body>
+</html> 
+
